d64671c05ee873c7ec321386d5a8575861d6843d
Tomasz Setkowski <tom@ai-traders.com>
2017-10-24 19:15:26 +0000

    setup build and test tasks

---
+++ tasks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+# PATCH currently failing private caching server
+rm -f ~/.nuget/NuGet/NuGet.Config
+
+dotnet restore
+
+command="$1"
+case "${command}" in
+  build)
+    dotnet publish -c Release src/LiGet.App/LiGet.App.csproj
+    dotnet publish -c Release tests/LiGet.Tests/LiGet.Tests.csproj
+    ;;
+  test)
+    mkdir -p tools
+    cd tools && nuget install xunit.runner.console -Version 2.3.0 && cd ..
+    test_assembly='tests/LiGet.Tests/bin/Release/netcoreapp2.0/publish/LiGet.Tests.dll'
+    dotnet tools/xunit.runner.console.2.3.0/tools/netcoreapp2.0/xunit.console.dll $test_assembly
+    ;;
+    *)
+      echo "Invalid command: '${command}'"
+      exit 1
+    ;;
+esac
