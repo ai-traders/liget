9c16285e8aafc58ea36677051303ce34839f7b08
Tomasz Setkowski <tom@ai-traders.com>
2017-10-25 14:29:55 +0000

    patch loading nancy modules during tests

--- tests/LiGet.Tests/TestBootstrapper.cs
+++ tests/LiGet.Tests/TestBootstrapper.cs
@@ -1,8 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
 using Autofac;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Bootstrappers.Autofac;
+using Xunit;
 
 namespace LiGet.Tests
 {
     public class TestBootstrapper : ProgramBootstrapper
     {
+        //PATCH: Force nancy modules to register, autodiscovery seems to fail when testing
+        public TestBootstrapper()
+            :base(b => {
+                b.RegisterType<PackagesNancyModule>().As<INancyModule>();
+            })
+        {            
+        }
     }
 }
